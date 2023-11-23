545c2214be07d08a4ad6f910d3240d11d8a8ee03
Christo Goosen <christogoosen@gmail.com>
2023-11-23 16:18:10 +0000

    Fix hugginface issue

--- llm_guard/llm_guard.py
+++ llm_guard/llm_guard.py
@@ -1,5 +1,7 @@
 import decimal
 import json
+import time
+
 import requests
 from app_config import settings
 from requests.adapters import HTTPAdapter, Retry
@@ -16,13 +18,25 @@ class LLMGaurdV1:
     def query(self, prompt):
         json_payload = {"inputs": prompt, "options": {"wait_for_model": True, "use_cache": True}}
         s = requests.Session()
-        retries = Retry(total=5, backoff_factor=5, status_forcelist=[502, 503, 504])
+        retries = Retry(total=5, backoff_factor=15, status_forcelist=[502, 503, 504])
         s.mount("https://", HTTPAdapter(max_retries=retries))
 
         response = s.post(
             LLMGaurdV1.API_URL, headers=LLMGaurdV1.headers, json=json_payload
         )
         resp_json = response.json(parse_float=decimal.Decimal)
+        try:
+            print(resp_json[0])
+            print("No Exception")
+        except Exception as e:
+            print("Exception")
+            time.sleep(5)
+            response = s.post(
+                LLMGaurdV1.API_URL, headers=LLMGaurdV1.headers, json=json_payload
+            )
+            resp_json = response.json(parse_float=decimal.Decimal)
+
         print(response.status_code)
         print(resp_json)
+
         return resp_json
