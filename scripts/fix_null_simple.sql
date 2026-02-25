8436786694b4dbd226315c3d71203c41b6f5569c
Christo Goosen <christogoosen@gmail.com>
2026-02-25 12:25:10 +0200

    Null script

--- scripts/fix_null_simple.sql
+++ scripts/fix_null_simple.sql
@@ -4,3 +4,5 @@
 UPDATE events SET input_transcription = NULL WHERE input_transcription IS NOT NULL AND (LOWER(TRIM(input_transcription)) = 'null' OR input_transcription = '');
 UPDATE events SET output_transcription = NULL WHERE output_transcription IS NOT NULL AND (LOWER(TRIM(output_transcription)) = 'null' OR output_transcription = '');
 
+
+
